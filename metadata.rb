d41b6addbc5b9a81b6655a45d7a4cda5d522f0a3
JJ Asghar <jj@chef.io>
2017-02-03 14:05:29 -0600

    typo in metadata

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'jj@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures chefdk'
 long_description 'Installs/Configures chefdk'
-version '1.0.0'
+version '1.0.1'
 
 issues_url 'https://github.com/jjasghar/chefdk-cookbook/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/chefdk-cookbook/chefdk' if respond_to?(:source_url)
+source_url 'https://github.com/jjasghar/chefdk-cookbook/' if respond_to?(:source_url)
 
 %w(ubuntu debian redhat centos).each do |os|
   supports os
